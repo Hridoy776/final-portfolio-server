324345ecfea1471964924c425037b8dd59d3fba9
Hridoy <rakibhasanhridoy776@gmail.com>
2022-08-30 02:57:52 +0600

    port added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors')
-const port = 5000;
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 //middlewere
 app.use(cors());
